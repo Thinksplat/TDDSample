28a8163c05c91d0f9b14745e5fcd9ed597fc6d29
John Aughey <jha@aughey.com>
2022-04-26 11:39:10 +0000

    Basic interface implementation complete

---
+++ CPlusPlus/lib/IIntegerProvider.h
@@ -0,0 +1,11 @@
+#ifndef FCB38476_20D6_4FB4_A0E4_8803A2E087A4
+#define FCB38476_20D6_4FB4_A0E4_8803A2E087A4
+
+#include <stdint.h>
+
+class IIntegerProvider {
+public:
+    virtual int16_t GetInteger() = 0;
+};
+
+#endif /* FCB38476_20D6_4FB4_A0E4_8803A2E087A4 */
--- CPlusPlus/tests/interface_tests.cpp
+++ CPlusPlus/tests/interface_tests.cpp
@@ -1,19 +1,46 @@
 #include <gtest/gtest.h>
 #include "lib/IBooleanProvider.h"
+#include "lib/IIntegerProvider.h"
 
 class MockBoolean : IBooleanProvider
 {
 public:
     MockBoolean(bool value) : value(value) {}
-    bool GetBool() { return value; }
+    bool GetBool() override { return value; }
+    void SetValue(bool value) { this->value = value; }
 private:
     bool value;
 };
 
+class MockInteger : IIntegerProvider {
+public:
+    MockInteger(int16_t value) : value(value) {}
+    int16_t GetInteger() override { return value; }
+    void SetValue(int16_t value) { this->value = value; }
+private:
+    int16_t value;
+};
+
 // Demonstrate some basic assertions.
-TEST(Interface, BasicInherientance)
+TEST(Interface, MockBooleanBasicInherientance)
 {
     MockBoolean mock(true);
     auto value = mock.GetBool();
     ASSERT_TRUE(value);
+
+    mock.SetValue(false);
+    value = mock.GetBool();
+    ASSERT_FALSE(value);
+}
+
+// Demonstrate some basic assertions.
+TEST(Interface, MockIntegerBasicInherientance)
+{
+    MockInteger mock(1);
+    auto value = mock.GetInteger();
+    ASSERT_EQ(value, 1);
+
+    mock.SetValue(2);
+    value = mock.GetInteger();
+    ASSERT_EQ(value, 2);
 }
