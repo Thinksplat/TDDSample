ca8a2be29cf9f322a10cf1ba81ef0b83ea1686ca
John Aughey <jha@aughey.com>
2022-04-26 17:48:25 +0000

    setting up proper configuration for self-hosted containers

--- CPlusPlus/watch-test.sh
+++ CPlusPlus/watch-test.sh
@@ -1,3 +1,5 @@
+mkdir build 2> /dev/null
+
 inotifywait -e close_write,moved_to,create -m tests lib shared |
 while read -r directory events filename; do
   (cd build && cmake .. && make && make test)
