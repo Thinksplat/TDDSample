36f5f99ecf687be4ef0fe9bb1b9c6bea611bb07d
John Aughey <jha@aughey.com>
2022-04-27 13:37:33 +0000

    firmware

--- CPlusPlus/PrototypeReader/main.cpp
+++ CPlusPlus/PrototypeReader/main.cpp
@@ -1,11 +1,31 @@
 #include "bsp_main.h"
 #include "lib/Stopwatch.h"
 
+void Blink(IIntegerConsumer &led, ITimeProvider &time, int count, uint32_t delay_microseconds)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        // ON
+        Stopwatch stopwatch(time);
+        while (stopwatch.ElapsedMicroseconds() < delay_microseconds)
+        {
+            led.Consume(1);
+        }
+
+        // Off
+        stopwatch.Reset();
+        while (stopwatch.ElapsedMicroseconds() < delay_microseconds)
+        {
+            led.Consume(0);
+        }
+    }
+}
+
 void run_program()
 {
     BSP bsp;
 
-    ITimeProvider &time = bsp.Time();
+   // ITimeProvider &time = bsp.Time();
     // IBooleanProvider &pin0 = bsp.Pin0();
 
     // Stopwatch stopwatch(time);
@@ -13,7 +33,8 @@ void run_program()
     // {
     // }
 
-    while(true) {
-        bsp.LED().Consume(time.GetMicroseconds() / 1000000 % 2 == 0);
+    while (true)
+    {
+        Blink(bsp.LED(), bsp.Time(), 5, 1000000);
     }
 }
