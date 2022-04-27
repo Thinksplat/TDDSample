d7ad7db37ae67848c1193ea3b97007754711deb4
John Aughey <jha@aughey.com>
2022-04-27 00:29:44 +0000

    Doing a full build on startup

--- CPlusPlus/watch-test.sh
+++ CPlusPlus/watch-test.sh
@@ -1,4 +1,5 @@
 mkdir build 2> /dev/null
+(cd build && cmake .. && make && make test)
 
 inotifywait -e close_write,moved_to,create -m tests lib shared |
 while read -r directory events filename; do
