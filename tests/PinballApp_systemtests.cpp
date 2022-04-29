d3d43bbdffb22f1ae74ff9198cace221bc88e020
John Aughey <jha@aughey.com>
2022-04-29 15:34:11 +0000

    system tests

--- tests/PinballApp_systemtests.cpp
+++ tests/PinballApp_systemtests.cpp
@@ -11,9 +11,12 @@
 // Kind of a heavy-weight testing framework to create an
 // entire simulated BSP.  This will create a known profile
 // and validate that the entire Pinball App runs as expected.
+
+
 class TestBSP : public IBSP
 {
 public:
+    static const int stable_time = 20 * 1000;
     TestBSP(IIntegerConsumer &consumer) 
     : consumer(consumer), sim(time), 
     time(10), // increment time by 10 microseconds each step
@@ -21,24 +24,23 @@ public:
     {
         // Setup a sample profile
         const int noise_time = 10;
-        const int stable_time = 20 * 1000; // 20ms
         PinballProfile::GenPinball(sim,
-                                   4 * 1000000, // 2 seconds in
-                                   stable_time * 3 / 2,
-                                   stable_time * 3 / 2,
+                                   4 * 1000000, // 4 seconds in
+                                   stable_time,
+                                   stable_time,
                                    noise_time, 0xd, 0xe);
         // Create another 6 seconds in
         PinballProfile::GenPinball(sim,
-                                   6 * 1000000, // 2 seconds in
-                                   stable_time * 3 / 2,
-                                   stable_time * 3 / 2,
+                                   6 * 1000000, // 6 seconds in
+                                   stable_time,
+                                   stable_time,
                                    noise_time, 42 & 0xf, 42 >> 4);
 
         // Create another 6.5 seconds in
         PinballProfile::GenPinball(sim,
-                                   6.5 * 1000000, // 2 seconds in
-                                   stable_time * 3 / 2,
-                                   stable_time * 3 / 2,
+                                   6.5 * 1000000, // 6.5 seconds in
+                                   stable_time,
+                                   stable_time,
                                    noise_time, 99 & 0xf, 99 >> 4);
     }
 
@@ -101,6 +103,7 @@ TEST(PinballApp, SetupAndRun)
 {
     typedef std::tuple<ITimeProvider::TimeType, int> Record;
     std::vector<Record> records;
+    const ITimeProvider::TimeType one_second = 1000000;
 
     // This is a wierd little circular dependency
     IBSP *bsp; // Need to do this in order to create the consumer and have access to the variable
@@ -114,10 +117,61 @@ TEST(PinballApp, SetupAndRun)
     TestBSP testbsp(consumer);
     bsp = &testbsp;
 
-    PinballApp::Run(*bsp,20*1000);
+    const uint32_t stable_time = TestBSP::stable_time;
+    PinballApp::Run(*bsp, stable_time * 2 / 3); // The stable time window we give the machine is 66% of the actual stable time
 
-    foreach(auto &value: records)
+    // The expected pattern should output values 
+    // 237, 42, and 99
+    // at times
+    // 4, 6, and 6.5 seconds.
+    // The last value should be around second 10
+    // There should be a value outputted at least every second.
+
+    const int time_range = 100; // Look for the value within +- timerange
+    auto expect_value = [&records](int value, ITimeProvider::TimeType time)
+    {
+        auto it = std::find_if(records.begin(), records.end(),
+                               [&time](const Record &r)
+                               {
+                                   return std::get<0>(r) > time-time_range && std::get<0>(r) < time+time_range;
+                               });
+        EXPECT_NE(it, records.end());
+        EXPECT_EQ(std::get<1>(*it), value);
+    };
+
+    for(auto &value: records)
     {
         std::cout << "Time: " << std::get<0>(value) << " Value: " << std::get<1>(value) << std::endl;
     }
+
+    expect_value(237, 4 * one_second + stable_time*2);
+    expect_value(42, 6 * one_second + stable_time*2);
+    expect_value(99, 6.5 * one_second + stable_time*2);
+
+    // Should be only three valid values
+    int validcount = 0;
+    for(auto &value: records)
+    {
+        if(std::get<1>(value) >= 0)
+        {
+            validcount++;
+        }
+    }
+
+    EXPECT_EQ(validcount, 3);
+
+    // The last value should be between time 10 and 11
+    EXPECT_NEAR(std::get<0>(records.back()), 10 * one_second, 11 * one_second);
+
+    // There should be at least one value every second
+    // (technically every second + the transmit time)
+    const uint32_t max_value_gap = one_second + stable_time * 3; // 3 times stable time to give it a fudge factor
+    uint32_t lasttime = 0;
+    for(auto &value: records)
+    {
+        uint32_t diff_from_last = std::get<0>(value) - lasttime;
+        EXPECT_LT(diff_from_last, max_value_gap);
+        lasttime = std::get<0>(value);
+    }
+
 }
