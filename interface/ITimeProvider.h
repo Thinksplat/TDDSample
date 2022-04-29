7d0f83ff66f6380d89415c94e51b9985547dca89
John Aughey <jha@aughey.com>
2022-04-29 03:42:13 +0000

    Formatting

--- interface/ITimeProvider.h
+++ interface/ITimeProvider.h
@@ -8,7 +8,7 @@ class ITimeProvider {
     public:
         typedef uint32_t TimeType;
         // Returns the number of microseconds since some arbitrary point in time.
-        // Rolls over after 2^32-1 microseconds or ~4300 seconds or ~71.5 minutes
+        // Rolls over after 2^32-1 microseconds or ~4300 seconds or ~71.5 minutes.
         virtual TimeType GetMicroseconds() = 0;
 };
 
