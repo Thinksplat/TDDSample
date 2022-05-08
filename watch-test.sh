5b3d33bc04c2fbb49933ec95557a4d34a1f424bd
John Aughey <jha@aughey.com>
2022-05-08 16:34:04 +0000

    fixing build

--- watch-test.sh
+++ watch-test.sh
@@ -1,9 +1,9 @@
 mkdir build 2> /dev/null
-(cd build && ninja && ./all_tests)
+(cd build && cmake .. -G Ninja && ninja && ./all_tests)
   echo "Done"
 
 inotifywait -e close_write,moved_to,create -m tests lib lib-behaviour shared bsp-linux interface |
 while read -r directory events filename; do
-  (cd build && cmake .. && ninja && ./all_tests)
+  (cd build && cmake .. -G Ninja && ninja && ./all_tests)
   echo "Done"
 done
