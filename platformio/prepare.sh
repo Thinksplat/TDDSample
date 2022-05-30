894929c5934ae256515b3366fe9f81acccd93311
John Aughey <jha@aughey.com>
2022-05-30 00:09:42 +0000

    trying rmeote stuff

--- platformio/prepare.sh
+++ platformio/prepare.sh
@@ -1,9 +1,11 @@
+mkdir include
+mkdir src
 PROJ=$1
 if [ -z "$PROJ" ]; then
   PROJ="PrototypePinball"
 fi
 rm -rf lib/* include/*
-cp ../$PROJ/main.cpp src
+cp ../$PROJ/main.cpp src/.
 cp -r ../lib include/.
 cp -r ../shared include/.
 cp -r ../lib-behaviour include/.
